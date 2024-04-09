24e6f0f826fcb03fb4b2f8f0eea68a92bc440c29
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-04-09 23:28:09 +0530

    bookstore upd

--- Frontend/src/Components/Course.jsx
+++ Frontend/src/Components/Course.jsx
@@ -6,7 +6,7 @@ const Course = () => {
     <div className='max w-screen-2xl container mx-auto md:px-20 px-4'>
        <div>
         <h1 className='text-2xl font-semibold md:text-4xl'>We're deligted to have you{" "}
-        <span className='text-pink-500'>Here!</span>
+        <span className='text-pink-500'>Here!!</span>
         </h1>
        </div>
     </div>
